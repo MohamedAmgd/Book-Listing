d1ac8ef2472f8e83b9a327d7f9ab82c1e2afe8bc
MohamedAmgd <50220171+MohamedAmgd@users.noreply.github.com>
2020-02-15 17:51:28 +0200

    Update Book.java

--- app/src/main/java/com/example/booklisting/Book.java
+++ app/src/main/java/com/example/booklisting/Book.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2020 Mohamed Amgd
+ *  Copyright 2020 Mohamed Amgd
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
